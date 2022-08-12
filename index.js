7f51bbc7bb60d5d7d9bcd82a4cbad5c7f8687bc2
Fasan Michael <fasan.michael1@gmail.com>
2022-08-12 09:43:11 -0400

    added projects

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const about = document.querySelector(".about");
 const btns = document.querySelectorAll(".tab-btn");
 const articles = document.querySelectorAll(".content");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
 
 about.addEventListener("click", (e) => {
   //   console.log(e.target.dataset.id);
@@ -19,3 +22,71 @@ about.addEventListener("click", (e) => {
     element.classList.add("active");
   }
 });
+
+// hamburger.addEventListener("click", () => {
+//   NavMenu.classList.toggle("active");
+// }
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+
+// navLink.forEach(link => {link.addEventListener("click", () => {
+
+navLink.forEach(n => {
+  n.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+    hamburger.classList.remove('active');
+  });
+});
+
+// const navLinks = document.querySelectorAll(".nav-link");
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     navMenu.classList.remove("active");
+//     hamburger.classList.remove("active");
+//   }
+// }
+// );
+
+// const navLinks = document.querySelectorAll(".nav-link");
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     navMenu.classList.remove("active");
+//     hamburger.classList.remove("active");
+//   }
+// }
+// );
+
+// const navLinks = document.querySelectorAll(".nav-link");
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     navMenu.classList.remove("active");
+//     hamburger.classList.remove("active");
+//   }
+// }
+// );
+
+// const navLinks = document.querySelectorAll(".nav-link");
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     navMenu.classList.remove("active");
+//     hamburger.classList.remove("active");
+//   }
+// }
+// );
+
+// const navLinks = document.querySelectorAll(".nav-link");
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     navMenu.classList.remove("active");
+//     hamburger.classList.remove("active");
+//   }
+// }
+// );
+
+// const navLinks = document.querySelectorAll(".nav-link");
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     navMenu.classList
